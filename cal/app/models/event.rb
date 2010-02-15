265c77bbe744dae33965d3c16fd22f179fd9d7b7
Theresa <the.resa@live.com>
2010-02-15 23:36:00 +0100

    changed models

--- cal/app/models/event.rb
+++ cal/app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ActiveRecord::Base
+  belongs_to :calendar
+
+  validates_presence_of :title
+  validates_length_of :title, :minimum => 6
+  validates_length_of :title, :maximum => 40
+
+
 end
